e663e855f44c1a026f08f23d9f5965b283181c06
Shaik Adam Durwaish <sa-52500885@safeaeon.com>
2025-10-22 05:46:38 -0700

    ig completed the backend part

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -1,6 +1,8 @@
 import User from "../models/User.js";
 import type { Request, Response, NextFunction } from "express";
 import jwt, { type JwtPayload } from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
 
 declare global {
     namespace Express {
@@ -15,18 +17,19 @@ declare global {
 
 export const AuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
     try{
-        const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
+        const token = req.headers.authorization?.split(' ')[1];
         if(!token) {
-            res.status(401).json({message: 'Token is missing'});
+            return res.status(401).json({message: 'Token is missing'});
         }
+        
         if(!process.env.JWT_SECRET) {
             return res.status(500).json({message: 'JWT Secret is missing'});
         }
         const decoded = jwt.verify(token,process.env.JWT_SECRET) as JwtPayload;
-
+        
         req.user = {id: decoded.id, username:decoded.username};
         next();
     }catch(err) {
-        return res.status(401).json({ message: "Invalid or expired token" });
+        return res.status(401).json({ message: "Invalid or expired token" ,err});
     }
 }
